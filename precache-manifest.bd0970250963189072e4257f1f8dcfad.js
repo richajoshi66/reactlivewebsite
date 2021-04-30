bf6f4666c11e98d8b61e5e1e3f5101b0ce1e6f91
richajoshi66 <richajoshi66@gmail.com>
2021-04-30 23:38:17 +0530

    Updates

---
+++ precache-manifest.bd0970250963189072e4257f1f8dcfad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b067800dc80a7aaf1a44c4dd67ae48a2",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "ce0729208c91e69e1b33",
+    "url": "/reactlivewebsite/static/css/2.d2d76128.chunk.css"
+  },
+  {
+    "revision": "5b930ba528db4a4aa560",
+    "url": "/reactlivewebsite/static/css/main.0d1ae658.chunk.css"
+  },
+  {
+    "revision": "ce0729208c91e69e1b33",
+    "url": "/reactlivewebsite/static/js/2.5c2f8bf7.chunk.js"
+  },
+  {
+    "revision": "84f5934c5e117a5dfe5534d2c2f19685",
+    "url": "/reactlivewebsite/static/js/2.5c2f8bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b930ba528db4a4aa560",
+    "url": "/reactlivewebsite/static/js/main.720128a3.chunk.js"
+  },
+  {
+    "revision": "42dff7df40042ba94213",
+    "url": "/reactlivewebsite/static/js/runtime-main.fa9f8398.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/reactlivewebsite/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/reactlivewebsite/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/reactlivewebsite/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/reactlivewebsite/static/media/slick.f97e3bbf.svg"
+  }
+]);
